d42296d1d0bb1f59b1fc5a6cdea7d0ac3dd419f4
Kalinin Yakov <z-chutvishechem@mail.ru>
2021-12-09 09:34:47 +0700

    Finish program

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int account = 100;
+        int payment = 2500;
+        int bonus = 1;
+        int total;
+        if (payment > 1000) {
+            System.out.println(total = payment / 100 * bonus + payment + account);
+        } else {
+        System.out.println(total = account + payment); } }
+
+}
